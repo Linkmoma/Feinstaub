0123be0f42dc560e743b78dfea6fafd2c52c1932
Linkmoma <82381213+Linkmoma@users.noreply.github.com>
2021-05-09 22:08:42 +0200

    Add files via upload
    Reader der nur Temperatur, Zeit und die Bonusaufgabe ausgibt

--- reader/reader/reader.py
+++ reader/reader/reader.py
@@ -2,13 +2,11 @@ import csv
 csv.list_dialects()
 ['excel', 'excel-tab', 'unix']
 
+extra_list = [] 
 
 reader = csv.reader(open("2021-04-26_dht22_sensor_3660.csv"), delimiter=";")
 for row in reader:
-    print(row)
-    ['sensor_id','sensor_type','location','lat','lon','timestamp','P1','durP1','ratioP1','P2','durP2','ratioP2']
-
-reader = csv.reader(open("2021-04-26_sds011_sensor_3659.csv"), delimiter=";")
-for row in reader:
-    print(row)
-    ['sensor_id','sensor_type','location','lat','lon','timestamp','P1','durP1','ratioP1','P2','durP2','ratioP2']
+    extra_list.append(row)
+    print('Zeit', row[5], 'tempreatur:', row[6], 'C')
+  
+print('\nBonusaufgabe (Liste aus Liste ausgeben)','\n', extra_list)
